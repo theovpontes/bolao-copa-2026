9ad1fc855c6503c3977ac1098213d26351179a9e
theovpontes <theovpontes@gmail.com>
2026-05-31 14:43:45 -0300

    Create format.ts

---
+++ lib/format.ts
@@ -0,0 +1,10 @@
+export function formatDateTimeBR(date: string | Date) {
+  return new Intl.DateTimeFormat("pt-BR", {
+    timeZone: "America/Sao_Paulo",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(new Date(date));
+}
